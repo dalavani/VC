243b0370c9c338109854e21a597aae8fd88ea164
dalavani <durgesh.alavani2@gmail.com>
2018-02-19 15:07:51 +0530

    Added Project
    ---

---
+++ VideoCommercial/VideoCommercial/Pages/Index.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace VideoCommercial.Pages
+{
+    public class IndexModel : PageModel
+    { 
+        private string folderLocation = "~\\..\\down\\commercial";
+        public void OnGet()
+        {
+            string[] filePaths = Directory.GetFiles(@folderLocation);
+            ViewData["Vids"] = filePaths;
+            ViewData["location"] = "..\\down\\commercial";
+        }
+    }
+}
